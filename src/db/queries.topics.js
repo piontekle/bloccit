fd64a8a30283272caa0663abe75c4ba0ebe35ee2
Lauren Piontek <piontekle@gmail.com>
2019-03-01 13:38:04 -0500

    Final topic_spec test failing, committed for review

---
+++ src/db/queries.topics.js
@@ -0,0 +1,14 @@
+const Topic = require("./models").Topic;
+
+module.exports = {
+
+  getAllTopics(callback){
+    return Topic.all()
+    .then((topics) => {
+      callback(null, topics);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+  }
+}
