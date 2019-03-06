27a436ac7506034b9b7ee7837a64462bd53b253d
Lauren Piontek <piontekle@gmail.com>
2019-03-05 21:43:39 -0500

    Checkpoint 9 Complete: Add Post CRUD functionality

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
     const staticRoutes = require("../routes/static");
     const topicRoutes = require("../routes/topics");
     const advertRoutes = require("../routes/advertisements");
+    const postRoutes = require("../routes/posts");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(advertRoutes);
+    app.use(postRoutes);
   }
 }
