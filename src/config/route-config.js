586fc6eb96dd67740e3a5a996dd78565ff66290b
Lauren Piontek <piontekle@gmail.com>
2019-03-08 15:42:03 -0500

    Checkpoint 11 complete: add User model and validation

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -5,11 +5,13 @@ module.exports = {
     const advertRoutes = require("../routes/advertisements");
     const postRoutes = require("../routes/posts");
     const flairRoutes = require("../routes/flair");
+    const userRoutes = require("../routes/users");
 
     app.use(staticRoutes);
     app.use(topicRoutes);
     app.use(advertRoutes);
     app.use(postRoutes);
     app.use(flairRoutes);
+    app.use(userRoutes);
   }
 }
