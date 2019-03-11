4bff7b86fb0df3c7bdd1f4a9f5f220d6eb2392e2
Lauren Piontek <piontekle@gmail.com>
2019-03-11 12:28:45 -0400

    Checkpoint 12 Authorization complete: add User to post relation and authorization to Topics

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,33 +1,44 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Topic", () => {
   beforeEach((done) => {
     this.topic;
     this.post;
-    sequelize.sync({force: true}).then((res) => {
-      Topic.create({
-        title: "Expeditions to Alpha Centauri",
-        description: "A compilation of reports from recent visits to the star system."
-      })
-      .then((topic) => {
-        this.topic = topic;
+    this.user;
 
-        Post.create({
-          title: "My first visit to Proxima Centauri b",
-          body: "I saw some rocks.",
-          topicId: this.topic.id
-        })
-        .then((post) => {
-          this.post = post;
-          done();
-        });
+    sequelize.sync({force: true}).then((res) => {
+
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
       })
-      .catch((err) => {
-        console.log(err);
-        done();
-      });
+      .then((user) => {
+        this.user = user;
+
+        Topic.create({
+          title: "Expeditions to Alpha Centauri",
+          description: "A compilation of reports from recent visitsto the star system.",
+
+          posts: [{
+            title: "My first visit to Proxima Centauri b",
+            body: "I saw some rocks.",
+            userId: this.user.id
+          }]
+        }, {
+          include: {
+            model: Post,
+            as: "posts"
+          }
+        })
+        .then((topic) => {
+          this.topic = topic;
+          this.post = topic.posts[0];
+          done();
+        })
+      })
     });
   });
 
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,5 +1,6 @@
 const Topic = require("./models").Topic;
 const Post = require("./models").Post;
+const Authorizer = require("../policies/topic");
 
 module.exports = {
 
@@ -38,33 +39,48 @@ module.exports = {
       callback(err);
     })
   },
-  deleteTopic(id, callback){
-    return Topic.destroy({
-      where: {id}
-    })
+  deleteTopic(req, callback){
+    return Topic.findById(req.params.id)
     .then((topic) => {
-      callback(null, topic);
+      const authorized = new Authorizer(req.user, topic).destroy();
+
+      if(authorized) {
+        topic.destroy()
+        .then((res) => {
+          callback(null, topic);
+        });
+      } else {
+        req.flash("notice", "You are not authorized to do that.")
+        callback(401);
+      }
     })
     .catch((err) => {
       callback(err);
-    })
+    });
   },
-  updateTopic(id, updatedTopic, callback){
-    return Topic.findById(id)
+  updateTopic(req, updatedTopic, callback){
+    return Topic.findById(req.params.id)
     .then((topic) => {
       if(!topic){
         return callback("Topic not found");
       }
 
-      topic.update(updatedTopic, {
-        fields: Object.keys(updatedTopic)
-      })
-      .then(() => {
-        callback(null, topic);
-      })
-      .catch((err) => {
-        callback(err);
-      });
+      const authorized = new Authorizer(req.user, topic).update();
+
+      if(authorized) {
+        topic.update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch((err) => {
+          callback(err);
+        });
+      } else {
+        req.flash("notice", "You are not authorized to do that.");
+        callback("Forbidden");
+      }
     });
   }
 
